14adb0be757e94a5abb0fd6b9dd9e7a8d326be38
Dasha Lary <dk2800@nyu.edu>
2020-09-21 17:06:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
 # if word =~ /\A[aeiou]/
-if word.start_with?(/[aeiou]/)
+if word.match(/[aeiou]/)
   return true 
 # elsif word.match(/\A(?=[^aeiou])(?=[a-z])/i)
 else 
