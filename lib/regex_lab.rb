da2b71c4b45d5c15e01293cfbe6144450e0754ae
Dasha Lary <dk2800@nyu.edu>
2020-09-21 17:50:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/\A[un]/) + text.scan(/)
+text.scan(/\A[un]/) + text.scan(/\z[ing]/)
 end
 
 def words_five_letters_long(text)
