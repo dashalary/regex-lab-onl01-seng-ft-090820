6c782805dec0ba4883c86ec4812bf3feaf9060f3
Dasha Lary <dk2800@nyu.edu>
2020-09-21 17:07:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,7 +2,7 @@ def starts_with_a_vowel?(word)
 # if word =~ /\A[aeiou]/
 if word.match(/[aeiou]/)
   return true 
-elsif word.match(/\A(?=[^aeiou])(?=[a-z])/i)
+elsif word.match(/(?=[^aeiou])(?=[a-z])/i)
 
   return false 
 end 
